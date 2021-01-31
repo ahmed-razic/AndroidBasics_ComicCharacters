6ca713b09864d010c7f734305a266c0c094d0367
ahmed-razic <ahmed.razic@gmail.com>
2021-01-31 01:16:40 +0100

    made blank activities and xmls

---
+++ app/src/main/java/com/example/android/comiccharacters/BonelliActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.comiccharacters;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BonelliActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_of_characters);
+    }
+}
